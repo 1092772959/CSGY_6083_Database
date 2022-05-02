ffb334bdeb0d212e2c9dbb17d57db907faa395c0
xiuwenli <xl3335@nyu.edu>
2022-05-02 01:46:15 -0400

    [FEAT] allow cross origin requests

--- CSGY-6083-Backend/src/main/java/com/backend/qa/config/WebConfig.java
+++ CSGY-6083-Backend/src/main/java/com/backend/qa/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.backend.qa.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,6 +17,16 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Autowired
     private AccessInterceptor interceptor;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH")
+                .maxAge(3600);
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
